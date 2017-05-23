151dc2418f657266fa7f6960ed9b5d9057764cba
Feather Knee <featherknee@gmail.com>
2017-05-22 17:30:41 -0700

    move character into separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 //import Speak from './utils/Speak'
-//import ReactTooltip from 'react-tooltip'
+import Character from './components/Character'
 import './App.css';
 
 class App extends Component {
@@ -15,11 +15,9 @@ class App extends Component {
     document.title = 'Scripted Demo'
   }
 
-  pickVoice(e) {
-    e.preventDefault()
+  pickVoice() {
     this.setState({
       characterHover: !this.state.characterHover,
-      character: parseInt(e.target.dataset.id, 10)
     })
   }
 
@@ -30,62 +28,55 @@ class App extends Component {
           <h2>Cinderella Must Die</h2>
         </div>
         <div className='screenplay'>
-
-          <div className='character'>
-            <div className='spacer'> </div>
-            <div data-id='1' className={(this.state.characterHover && this.state.character === 1) ? 'highlight' : ''} onClick={(e) => this.pickVoice(e)}>
-            CINDERELLA
-            </div>
-            <div className='spacer'> </div>
-          </div>
+          <Character
+            name={'CINDERELLA'}
+            character={1}
+            action={() => this.pickVoice()}
+            characterHover={this.state.characterHover}
+            />
           <div className='dialogue'>
           My father was a baron, my mother a baroness. I'm of noble blood!
           </div>
-          <div className='character'>
-            <div className='spacer'> </div>
-            <div data-id='2' className={(this.state.characterHover && this.state.character === 2) ? 'highlight' : ''} onClick={(e) => this.pickVoice(e)}>
-            MADEMOISELLE
-            </div>
-            <div className='spacer'> </div>
-          </div>
+          <Character
+            name={'MADEMOISELLE'}
+            character={2}
+            action={() => this.pickVoice()}
+            characterHover={this.state.characterHover}
+            />
           <div className='dialogue'>
           You fool. I <bold>know</bold> your mother.
           </div>
-          <div className='character'>
-            <div className='spacer'> </div>
-            <div data-id='1' className={(this.state.characterHover && this.state.character === 1) ? 'highlight' : ''} onClick={(e) => this.pickVoice(e)}>
-            CINDERELLA
-            </div>
-            <div className='spacer'> </div>
-          </div>
-          <div className='spacer'> </div>
+          <Character
+            name={'CINDERELLA'}
+            character={1}
+            action={(e) => this.pickVoice(e)}
+            characterHover={this.state.characterHover}
+            />
           <div className='dialogue'>
           She's not my mother! She's my stepmother. My father married her after my real mother died.
           </div>
-          <div className='character'>
-            <div className='spacer'> </div>
-            <div data-id='2' className={(this.state.characterHover && this.state.character === 2) ? 'highlight' : ''} onClick={(e) => this.pickVoice(e)}>
-            MADEMOISELLE
-            </div>
-            <div className='spacer'> </div>
-          </div>
+          <Character
+            name={'MADEMOISELLE'}
+            character={2}
+            action={(e) => this.pickVoice(e)}
+            characterHover={this.state.characterHover}
+            />
           <div className='dialogue'>
           You're telling tales. What about your sisters?
           </div>
-          <div className='character'>
-            <div className='spacer'> </div>
-            <div data-id='1' className={(this.state.characterHover && this.state.character === 1) ? 'highlight' : ''} onClick={(e) => this.pickVoice(e)}>
-            CINDERELLA
-            </div>
-            <div className='spacer'> </div>
-          </div>
+          <Character
+            name={'CINDERELLA'}
+            character={1}
+            action={(e) => this.pickVoice(e)}
+            characterHover={this.state.characterHover}
+            />
           <div className='dialogue'>
           They're not my sisters, they're my stepsisters. When my father dies they squandered my inheritance and force me into service.
           But, as God is my witness, I'm one of you.
           </div>
         </div>
-      </div>
-    );
+    </div>
+    )
   }
 }
 
